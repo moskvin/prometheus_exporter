c646cda67e3479dc5c268e7aed0df37ac5a2ae20
Nikolay Moskvin <nikolay.moskvin@gmail.com>
2023-04-26 12:24:36 +1000

    bump version and fixup for undefined method `define_methods_on_module' for PrometheusExporter::Instrumentation::MethodProfiler

--- lib/prometheus_exporter/instrumentation/method_profiler.rb
+++ lib/prometheus_exporter/instrumentation/method_profiler.rb
@@ -47,11 +47,35 @@ class PrometheusExporter::Instrumentation::MethodProfiler
 
   def self.stop
     finish = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-    if data = Thread.current[:_method_profiler]
+    data = Thread.current[:_method_profiler]
+    if data
       Thread.current[:_method_profiler] = nil
       start = data.delete(:__start)
       data[:total_duration] = finish - start
     end
     data
+  end
+
+  def self.define_methods_on_module(klass, methods, name)
+    patch_source_line = __LINE__ + 3
+    patches = methods.map do |method_name|
+      <<~RUBY
+        def #{method_name}(*args, &blk)
+          unless prof = Thread.current[:_method_profiler]
+            return super
+          end
+          begin
+            start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+            super
+          ensure
+            data = (prof[:#{name}] ||= {duration: 0.0, calls: 0})
+            data[:duration] += Process.clock_gettime(Process::CLOCK_MONOTONIC) - start
+            data[:calls] += 1
+          end
+        end
+      RUBY
+    end.join("\n")
+
+    klass.module_eval patches, __FILE__, patch_source_line
   end
 end
